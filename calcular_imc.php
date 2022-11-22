f20a34c2b82cdbea3b81615c2c72ebedcad60523
Sergio Abarca <48446450+sergioab1989@users.noreply.github.com>
2022-11-22 14:57:50 -0300

    Update APP
    se ingreso link para volver

--- calcular_imc.php
+++ calcular_imc.php
@@ -25,8 +25,12 @@
                 <p>Su estatura es de ".$estatura," metros</p>		
                 <p>Su peso es de ".$peso," kilos</p>		
                 <p>IMC = ",$peso,"/",$estatura,"² = ",$imc,"</p>		
-                <p>Su diagnostico es: ", DiagnosticoIMC($imc),"</p>		
-            </div>	
+                <p>Su diagnostico es: ", DiagnosticoIMC($imc)"</p>		
+		
+            </div>
+	    <div>
+                <a href="index.php">Regresar al INICIO</a>		
+            </div>
           </article>";
     ?>
 </body>
